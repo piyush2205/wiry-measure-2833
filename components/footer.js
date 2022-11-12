1310f26cda080496b3c788e2a1fbd6ead8bb1303
piyush2205 <pk.rv.668@gmail.com>
2022-11-12 23:49:25 +0530

    fw19_1048_day3_footer

---
+++ components/footer.js
@@ -0,0 +1,52 @@
+let footer=()=>{
+return`<div id="ps_footer_1">
+<div class="footer_div1_ps"><img src="./tripguru.png" alt="">
+<h1 style="font-size: 12px;border-bottom: 1px solid #DCDFE0;padding: 10px;margin-top: -15px;margin-left: 5px;">India's Largest Travel Community</h1>
+<img   style="margin-top: 10px;margin-left: 30px;" src="./Web capture_12-11-2022_12423_www.tripoto.com.jpeg" alt="">
+     <img style="margin-top: 20px;margin-left: 40px;width: 40%; "src="https://cdn1.tripoto.com/assets/2.9/img/logo/download-android-app.svg" alt=""></div>
+<div class="footer_div2_ps">
+    <div>About Tripguru</div>
+    <div><a href="" >How It Works</a></div>
+    <div><a href="" >   FAQ’s</a></div>
+    <div><a href="" >  Privacy Policy</a></div>
+    <div><a href="" > Terms & Conditions</a></div>
+    <div><a href="" > Careers</a></div>
+    <div><a href="" >  Contact Us</a></div>
+   
+        
+</div>
+<div class="footer_div2_ps">
+    <div> Products</div>
+    <div><a href="" >   Tour Packages</a></div>
+    <div><a href="" > Travel Guides</a></div>
+    <div><a href="" > Trips & Itineraries</a></div>
+    <div><a href="" > Weekend Getaways</a></div>
+    <div><a href="" >Places to Visit</a></div>
+    <div><a href="" >Other Travel Categories</a></div>
+    <div><a href="" > Triguru Forum</a></div>
+    <div><a href="" > Honeymoon Packages</a></div>
+        
+</div> <div class="footer_div2_ps">
+    <div>  Important Travel Links</div>
+    <div><a href="" >  Goa Tourism</a></div>
+    <div><a href="" > Dubai Tourism</a></div>
+    <div><a href="" >  Singapore Tourism</a></div>
+    <div><a href="" >Kerala Tourism</a></div>
+    <div><a href="" >Manali Tourism</a></div>
+    <div><a href="" >Bali Tourism</a></div>
+    <div><a href="" >   Sri Lanka Tourism</a></div>
+    <div><a href="" >   Honeymoon Destinations In India</a></div>
+        
+</div> <div class="footer_div2_ps">
+    <div>Partner Programs</div>
+    <div><a href="" >  Buy Travel Leads</a></div>
+    <div><a href="" >  Partner With Us</a></div>
+    <div><a href="" >Earn Credits</a></div>
+    <div><a href="" >  Get Paid To Travel</a></div>
+    <div><a href="" >  Import Blog To Itinerary</a></div>
+</div>
+</div>
+<p style="text-align:center  ; padding: 50px;font-size: 12px;">© All rights reserved.</p>`
+}
+
+export default footer
---
+++ components/navbar2.js
@@ -0,0 +1,84 @@
+let navbar2=()=>{
+    return`<div id="ps_navbar" style="background-color: #2f9bdb;">
+    <div class="ps_tripguru"><a href=""> <img src="./tripguru.png" alt=""></a></div>
+    <div id="query_ps_div">
+        <input type=" text" id="query_ps"
+        placeholder="Search for itineraries, destinations, hotels or activities">
+        <img id="_search_icon_ps"
+            src="data:image/svg+xml,%3csvg viewBox='0 0 34 32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' height='16px' width='17px'%3e %3cpath style='fill:%23506066' d='M20.571 14.857q0-3.304-2.348-5.652t-5.652-2.348-5.652 2.348-2.348 5.652 2.348 5.652 5.652 2.348 5.652-2.348 2.348-5.652zM29.714 29.714q0 0.929-0.679 1.607t-1.607 0.679q-0.964 0-1.607-0.679l-6.125-6.107q-3.196 2.214-7.125 2.214-2.554 0-4.884-0.991t-4.018-2.679-2.679-4.018-0.991-4.884 0.991-4.884 2.679-4.018 4.018-2.679 4.884-0.991 4.884 0.991 4.018 2.679 2.679 4.018 0.991 4.884q0 3.929-2.214 7. 125l6.125 6.125q0.661 0.661 0.661 1.607z'%3e %3c/path%3e %3c/svg%3e"
+            alt="">
+    </div>
+    <div id="ps_li_ui">
+        <div class="ps_div_1">
+            <li class="ps_inspiration" style="cursor:pointer ;"><a>Inspirations <img
+                        src="data:image/svg+xml,%3csvg version='1.1' xmlns='http://www.w3.org/2000/svg' width='8' height='12' viewBox='0 0 8 14'%3e %3cpath style='fill:white%3b' d='M8 5.5q0 0.203-0.148 0.352l-3.5 3.5q-0.148 0.148-0.352 0.148t-0.352-0.148l-3.5-3.5q-0.148-0.148-0.148-0.352t0.148-0.352 0.352-0.148h7q0.203 0 0.352 0.148t0.148 0.352z'%3e %3c/path%3e %3c/svg%3e"
+                        alt="caretdown" style="height: 12px; width: 8px;"></a>
+                <ul class="dropdown_inspiration">
+                    <div>
+                        <li><a href="/singapore">Visit Singapore</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/beaches">Beaches</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/mountains">Mountains</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/heritage">Heritage</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/weekend-guide">Weekend Guide</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/festivals-of-india">Upcoming Festivals</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/honeymoon-packages">Honeymoon Packages</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/wildlife-tourism">Wildlife Tourism</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/road-trips">Road Trips</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/getaways-guide">Getaways Guide</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/luxury-travel">Luxury Travel</a></li>
+                    </div>
+                    <div>
+                        <li><a href="/explore-more">Explore More</a></li>
+                    </div>
+                </ul>
+            </li>
+        </div>
+        <div class="ps_div_2">
+            <li><a href="/community">Forum</a></li>
+        </div>
+        <div class="ps_div_3">
+            <li><a href="/tour-packages">Packages</a></li>
+        </div>
+        <div class="ps_div_4">
+            <li class="ps_publish"><a>Publish Trip&nbsp;<img
+                        src="data:image/svg+xml,%3csvg version='1.1' xmlns='http://www.w3.org/2000/svg' width='8' height='12' viewBox='0 0 8 14'%3e %3cpath style='fill:white%3b' d='M8 5.5q0 0.203-0.148 0.352l-3.5 3.5q-0.148 0.148-0.352 0.148t-0.352-0.148l-3.5-3.5q-0.148-0.148-0.148-0.352t0.148-0.352 0.352-0.148h7q0.203 0 0.352 0.148t0.148 0.352z'%3e %3c/path%3e %3c/svg%3e"
+                        alt="caretdown" style="height: 12px; width: 8px;"></a>
+                <ul class="dropdown_publish">
+                    <div>
+                        <li><a href="">Create New</a>
+                    </div>
+                    <div>
+                        <li><a href="">Upload Photos/Videos</a>
+                    </div>
+                    <div>
+                        <li><a href="">Import Blog</a></li>
+                    </div>
+                </ul>
+            </li>
+        </div>
+        <div class="ps_div_5"><a href="">Sign in</a></div>
+    </div>
+</div>
+   
+`
+}
